90f0cc1f81fe424406dd94e48d8ab2d5af6d024f
iFh4jz <iFh4jz@gmail.com>
2018-01-24 23:16:12 +0800

    Update imwrite.cpp

--- opencv3/imwrite.cpp
+++ opencv3/imwrite.cpp
@@ -6,37 +6,37 @@ using namespace std;
 
 void createAlphaMat(Mat &mat)
 {
-	for(int i = 0; i < mat.rows; i++) 
+	for (int i = 0; i < mat.rows; i++)
 	{
-		for(int j = 0; j < mat.cols; ++j)
+		for (int j = 0; j < mat.cols; ++j)
 		{
-			Vec4b&rgba = mat.at<vec4b>(i, j);
+			Vec4b&rgba = mat.at<Vec4b>(i, j);
 			rgba[0] = UCHAR_MAX;
-			rgba[1] = satrue_cast<uchar>((float (mat.cols - j))/
-					((float)mat.cols) * UCHAR_MAX);
-			rgba[2] = satrue_cast<uchar>((float (mat.rows - i))/
-					((float)mat.rows) * UCHAR_MAX);
-			rgba[3] = satrue_cast<uchar>(0.5 * (rgba[1] + rgba[2]));
+			rgba[1] = saturate_cast<uchar>((float(mat.cols - j)) /
+				((float)mat.cols) * UCHAR_MAX);
+			rgba[2] = saturate_cast<uchar>((float(mat.rows - i)) /
+				((float)mat.rows) * UCHAR_MAX);
+			rgba[3] = saturate_cast<uchar>(0.5 * (rgba[1] + rgba[2]));
 		}
 	}
 }
 
 int main()
 {
-	//´´½¨´øAlphaÍ¨µÀµÄMat
+	//åˆ›å»ºå¸¦Alphaé€šé“çš„Mat
 	Mat mat(480, 640, CV_8UC4);
 	createAlphaMat(mat);
 	vector<int>compression_params;
-	compression_params.push_back(CV_IMWRITE_COMPRESSION);
+	compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
 	compression_params.push_back(9);
-	try{
-		imwrite("Í¸Ã÷AlphaÖµÍ¼.png", mat, compression_params);
-		imshow("Éú³ÉµÄPNGÍ¼",mat);
-		fprintf(stdout, "PNGÎÄ¼şµÄalphaÊı¾İ±£´æÍê±Ï\n");
+	try {
+		imwrite("é€æ˜Alphaå€¼å›¾.png", mat, compression_params);
+		imshow("ç”Ÿæˆçš„PNGå›¾", mat);
+		fprintf(stdout, "PNGæ–‡ä»¶çš„alphaæ•°æ®ä¿å­˜å®Œæ¯•\n");
 		waitKey(0);
 	}
-	catch(runtime_error& ex) {
-		fprintf(stderr, "Í¼Ïñ×ª»»³ÉPNG¸ñÊ½·¢Éú´íÎó£º%s\n", ex.what());
+	catch (runtime_error& ex) {
+		fprintf(stderr, "å›¾åƒè½¬æ¢æˆPNGæ ¼å¼å‘ç”Ÿé”™è¯¯ï¼š%s\n", ex.what());
 		return 1;
 	}
 	return 0;
